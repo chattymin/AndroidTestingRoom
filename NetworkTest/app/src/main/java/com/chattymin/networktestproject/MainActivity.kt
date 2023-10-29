a8b616e726339fc7ff3c6c2e82cf60d96e0e8b7f
Dongmin <pdm001125@naver.com>
2023-10-29 19:23:44 +0900

    feat: ViewBinding 연결

--- NetworkTest/app/src/main/java/com/chattymin/networktestproject/MainActivity.kt
+++ NetworkTest/app/src/main/java/com/chattymin/networktestproject/MainActivity.kt
@@ -2,10 +2,19 @@ package com.chattymin.networktestproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.chattymin.networktestproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (NetworkManager.checkNetworkState(this)){
+            binding.textView.text = "Network is available"
+        } else {
+            binding.textView.text = "Network is not available"
+        }
     }
 }

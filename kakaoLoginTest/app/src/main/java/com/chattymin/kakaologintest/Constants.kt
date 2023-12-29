0428151ede339417908ea1c5bceecbefbcbef780
Dongmin <pdm001125@naver.com>
2023-12-29 14:38:43 +0900

    [ADD] 기초 셋팅

---
+++ kakaoLoginTest/app/src/main/java/com/chattymin/kakaologintest/Constants.kt
@@ -0,0 +1,6 @@
+package com.chattymin.kakaologintest
+
+object Constants {
+    const val TAG = "YOUR TAG"
+    const val APP_KEY = "YOUR NATIVE_APP_KEY"
+}
--- kakaoLoginTest/app/src/main/java/com/chattymin/kakaologintest/MainActivity.kt
+++ kakaoLoginTest/app/src/main/java/com/chattymin/kakaologintest/MainActivity.kt
@@ -2,8 +2,10 @@ package com.chattymin.kakaologintest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.chattymin.kakaologintest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
